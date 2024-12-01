5bbb86d0325b86533dd3c250834519340694cb18
Alan Christofer <webxspark@gmail.com>
2024-12-01 23:52:08 +0530

    New: Front-end setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { BackgroundLines } from './components/ui/background-lines'
+
+function App() {
+  return(
+    <>
+      <BackgroundLines />
+    </>
+  )
+}
+
+export default App

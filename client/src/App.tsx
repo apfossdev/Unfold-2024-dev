9948db6d7e0ada3cd02d1a6fa123a09b6de7b3f3
Alan Christofer <webxspark@gmail.com>
2024-12-02 01:40:44 +0530

    JSX to TSX

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
     </>
   )
 }
